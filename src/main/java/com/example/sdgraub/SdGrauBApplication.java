ed728016c2b7038c520dd288f72c1cb004c05adf
Rafael Stefani Baptista <rafael.baptista@cwi.com.br>
2022-11-15 23:05:03 -0300

    teste

---
+++ src/main/java/com/example/sdgraub/SdGrauBApplication.java
@@ -0,0 +1,13 @@
+package com.example.sdgraub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdGrauBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdGrauBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sdgraub/SdGrauBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sdgraub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdGrauBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
